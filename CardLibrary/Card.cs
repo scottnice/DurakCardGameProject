ed8f48692ca608f9d01e785d722ed9671a11cc7a
Scott Nice <scottnice@live.com>
2014-03-05 15:25:23 -0500

    changed folder name from DurakLibrary to CardLibrary

---
+++ CardLibrary/Card.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace CardLibrary
+{
+    public class Card : ICloneable, IComparable
+    {
+        // max and min possible card values
+        private const int MIN_VALUE = (int)CardValue.TWO;
+        private const int MAX_VALUE = (int)CardValue.ACE;
+
+        private Suit mySuit; //public for now
+        private CardValue myValue;
+
+    #region "Propeties"
+
+        public Suit getSuit
+        {
+            get { return mySuit; }
+        }
+
+        public CardValue getValue
+        {
+            get {return myValue; }
+        }
+
+
+    #endregion
+
+
+    #region "Constructors"
+        public Card(Suit aSuit, CardValue cardNum) 
+        {
+            if ((int)cardNum >= MIN_VALUE && (int)cardNum <= MAX_VALUE)
+            {
+                mySuit = aSuit;
+                myValue = cardNum;
+            }
+            else
+                throw new System.ArgumentException("Card Value out of range", cardNum.ToString());
+        }
+    #endregion
+
+    #region "Methods"
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentException("Cannot compare card to null.");
+
+            Card aCard = obj as Card;
+            if (aCard != null)
+                return mySuit.CompareTo(aCard.mySuit)*100 + myValue.CompareTo(aCard.myValue);
+            else
+                throw new ArgumentException("Cannot compare card to non-card object.");
+        }
+
+        public object Clone()
+        {
+            return (Card)this.MemberwiseClone();
+        }
+    }
+    #endregion
+
+}
--- DurakProject/Enums.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace CardLibrary
-{
-    // the different suits a card can be
-    public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES };
-
-    public enum CardValue { TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
-}
